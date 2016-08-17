ecfa6360910b24c971e29cb7bd76bd3749afa462
杨恺 <njustyangkai@126.com>
2016-08-17 23:47:47 +0800

    url

--- app/util/util.js
+++ app/util/util.js
@@ -1,14 +1,16 @@
 define(function (require) {
     var sidebarChange = function (a) {
         if (!$(a).parent().is('.treeview')) {
-            var $ul = $(a).parent().parent();
-            if ($ul.is('.treeview-menu')) {
-                $ul.children('li').removeClass('active');
-            }
-            $('.sidebar li:not(.treeview) > a').parent().removeClass('active');
-            $(a).parent().addClass('active');
             var url = $(a).attr('href');
-            console.log(url);
+            url = url.substring(2, url.length);
+            $('div.content-wrapper').load('app/'+url +'/'+url+'.html?v=' + version,function() {
+                var $ul = $(a).parent().parent();
+                if ($ul.is('.treeview-menu')) {
+                    $ul.children('li').removeClass('active');
+                }
+                $('.sidebar li:not(.treeview) > a').parent().removeClass('active');
+                $(a).parent().addClass('active');
+            });
         }
     };
 
