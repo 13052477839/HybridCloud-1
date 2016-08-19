d6f063036776bae64da33e25de46dbe08147f21d
杨恺 <njustyangkai@126.com>
2016-08-19 08:47:54 +0800

    包

---
+++ app/volume/overview.js
@@ -0,0 +1,26 @@
+define(function(require, exports, module){
+
+    function Overview(){}
+
+    module.exports = Overview;
+
+    //==============================
+    // init
+    //==============================
+    Overview.prototype.init = function () {
+        /*$("#table1").bootstrapTable({
+            url: 'api/rest/table1',
+
+
+        });
+
+        $.ajax({
+            url: 'rest/test',
+            type: 'get',
+            dataType: 'json',
+            success: function(result) {
+                console.log(result);
+            }
+        })*/
+    }
+});
