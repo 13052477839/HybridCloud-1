1c398111deaf1c7f07add07aa12bf851f57c65f1
杨恺 <njustyangkai@126.com>
2016-08-21 10:49:01 +0800

    dialog

--- app/index/index.js
+++ app/index/index.js
@@ -13,6 +13,7 @@ define(function(require, exports, module) {
     Index.prototype.init = function() {
         $('header.main-header').load('app/header/header.html?v=' + version);
         $('aside.main-sidebar').load('app/sider/sider.html?v=' + version);
+        $('aside#modal').load('app/util/dialog.html?v=' + version);
         //$('footer.main-footer').load('app/footer/footer.html');
     }
 });
--- app/user/user.js
+++ app/user/user.js
@@ -12,6 +12,7 @@ define(function (require, exports, module) {
     //==============================
     User.prototype.init = function () {
         this.userTable();
+        this.delete();
         $('button, input').tooltip();
     };
 
@@ -46,10 +47,62 @@ define(function (require, exports, module) {
         }));
         $('.bootstrap-table .search input').attr('placeholder', '')
             .parent().append('<span></span>');
-        $('.fixed-table-container').append('<div class="fixed-table-footerButtons"><button disabled>删除</button></div>');
+        $('.fixed-table-container').append('<div class="fixed-table-footerButtons"><button disabled id="btn-user-delete">删除</button></div>');
         $table.on('check.bs.table uncheck.bs.table ' +
             'check-all.bs.table uncheck-all.bs.table', function () {
             $('.fixed-table-footerButtons button').prop('disabled', !$table.bootstrapTable('getSelections').length);
+        });
+    };
+
+    //==============================
+    // delete user
+    //==============================
+    User.prototype.delete = function () {
+        $('#btn-user-delete').click(function () {
+            var selections = $('#userTable').bootstrapTable('getSelections');
+            if (selections.length == 0) {
+                $('#dialog-alert p').text('未选择！');
+                $('#dialog-alert').modal({
+                    backdrop : true,
+                    keyboard : true,
+                    show : true
+                });
+            } else {
+                $('#dialog-confirm p').text('确认删除所选用户及其关联账号吗？');
+                $('#dialog-confirm').modal({
+                    backdrop: true,
+                    keyboard: true,
+                    show: true
+                });
+                $('#dialog-confirm .btn-confirm').unbind().click(function () {
+                    var ids = '';
+                    $.each(selections, function (i, v) {
+                        ids += v.id + ',';
+                    });
+                    ids = ids.substr(0, ids.length - 1);
+                    $('#dialog-confirm').modal('hide');
+                    $.ajax({
+                        url: API_URL.USERS,
+                        data: {
+                            'ids': ids
+                        },
+                        type: 'delete',
+                        dataType: 'json',
+                        success: function (result) {
+                            if (result.success) {
+                                $('#userTable').bootstrapTable('refresh');
+                            } else {
+                                $('#dialog-confirm p').text('删除失败');
+                                $('#dialog-confirm').modal({
+                                    backdrop: true,
+                                    keyboard: true,
+                                    show: true
+                                });
+                            }
+                        }
+                    });
+                });
+            }
         });
     }
 });
