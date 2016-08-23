32015198ac692a2f4c6657c7c773d8e893433876
杨恺 <njustyangkai@126.com>
2016-08-23 22:53:25 +0800

    create instance page

---
+++ app/instance/create.js
@@ -0,0 +1,15 @@
+define(function(require, exports, module){
+
+    var Util = require('util/util');
+
+    function Create(){}
+
+    module.exports = Create;
+
+    //=================================
+    // init
+    //=================================
+    Create.prototype.init = function(){
+        
+    };
+});
