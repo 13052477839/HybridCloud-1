2d2362a95e5bb027d044be55f682cf546f49d27e
杨恺 <njustyangkai@126.com>
2016-08-26 12:05:33 +0800

    header

--- app/header/header.js
+++ app/header/header.js
@@ -1,20 +1,57 @@
-define(function(require, exports, module) {
-    
-    function Header(){}
+define(function (require, exports, module) {
+    var Util = require('util/util');
+
+    function Header() {
+    }
 
     module.exports = Header;
 
     //================================
     // init
     //================================
-    Header.prototype.init = function() {
-        
+    Header.prototype.init = function () {
+        this.theme();
+        this.feedback();
+        this.help();
+        this.user();
+    };
+
+    //================================
+    //  theme
+    //================================
+    Header.prototype.theme = function () {
+        $('#theme').click(function () {
+            if ($('body').is('.skin-blue-light')) {
+                $('body').removeClass('skin-blue-light').addClass('skin-blue');
+            } else {
+                $('body').removeClass('skin-blue').addClass('skin-blue-light');
+            }
+        });
     };
 
     //================================
     //  feedback
     //================================
-    Header.prototype.feedback = function() {
-        
+    Header.prototype.feedback = function () {
+        $('#feedback').click(function () {
+            Util.alertDialog('敬请期待！');
+        });
+    };
+
+    //================================
+    //  help
+    //================================
+    Header.prototype.help = function () {
+        $('#help').click(function () {
+            Util.alertDialog('敬请期待！');
+        });
+    };
+
+    //================================
+    //  user
+    //================================
+    Header.prototype.user = function () {
+        var userName = window.localStorage.userName;
+        $('.user-menu > a span:first').html(userName);
     };
 });
